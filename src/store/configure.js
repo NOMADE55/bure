f26a32b3221d842fcfd90d228ec636441f733e75
Lucas G. Terracino <Lucas.T@geopagos.com>
2020-10-27 11:43:49 -0300

    ✨ Refactor store, removing sagas

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,42 +1,27 @@
 // https://github.com/diegohaz/arc/wiki/Redux-modules
 import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-// import { isDev, isBrowser } from 'config';
-import middlewares from './middlewares';
 import reducer from './reducer';
-import sagas from './sagas';
+import thunk from 'redux-thunk';
 
-console.log(process.env);
+// console.log(process.env);
 
-const devtools = window.devToolsExtension && process.env.NODE_ENV == "development"
+const devtools = window.devToolsExtension && process.env.NODE_ENV === "development"
   ? window.devToolsExtension
   : () => (fn) => fn;
 
-const configureStore = (initialState, services = {}) => {
-  const sagaMiddleware = createSagaMiddleware()
+const configureStore = (initialState) => {
 
   const enhancers = [
-    applyMiddleware(
-      ...middlewares,
-      sagaMiddleware
-    ),
+    applyMiddleware(thunk),
     devtools(),
   ]
 
   const store = createStore(reducer, initialState, compose(...enhancers))
-  let sagaTask = sagaMiddleware.run(sagas, services)
 
   if (module.hot) {
     module.hot.accept('./reducer', () => {
       const nextReducer = require('./reducer').default
       store.replaceReducer(nextReducer)
-    })
-    module.hot.accept('./sagas', () => {
-      const nextSagas = require('./sagas').default
-      sagaTask.cancel()
-      sagaTask.done.then(() => {
-        sagaTask = sagaMiddleware.run(nextSagas, services)
-      })
     })
   }
 
